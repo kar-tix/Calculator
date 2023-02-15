aacf1f6e577f3c1d4db27f7bf22e56d4c413c7e3
kar-tix <karolina_barylak01@spoko.pl>
2023-02-15 01:31:17 +0100

    Project

---
+++ Calculator/Form1.cs
@@ -0,0 +1,15 @@
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
